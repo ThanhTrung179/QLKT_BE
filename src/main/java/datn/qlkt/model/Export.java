c766cb9529d56d5704e3728fed52a41443c69f59
VU THANH TRUNG <54615459+trungquanadg@users.noreply.github.com>
2023-05-10 01:16:20 +0700

    update danh sach user

--- src/main/java/datn/qlkt/model/Export.java
+++ src/main/java/datn/qlkt/model/Export.java
@@ -22,15 +22,19 @@ public class Export {
     private String note;
     @OneToMany(mappedBy = "export", cascade = CascadeType.ALL, orphanRemoval = true)
     private Collection<WareHouseExport> wareHouseExports;
+    private Long moneyTotal;
     private Integer is_active;
+    private String creator;
 
-    public Export(Long id, String idExport, Date inTime, String note, Collection<WareHouseExport> wareHouseExports, Integer is_active) {
+    public Export(Long id, String idExport, Date inTime, String note, Collection<WareHouseExport> wareHouseExports, Long moneyTotal, String creator, Integer is_active) {
         this.id = id;
         this.idExport = idExport;
         this.inTime = inTime;
         this.note = note;
         this.wareHouseExports = wareHouseExports;
+        this.moneyTotal = moneyTotal;
         this.is_active = is_active;
+        this.creator = creator;
     }
 
     public Export() {
