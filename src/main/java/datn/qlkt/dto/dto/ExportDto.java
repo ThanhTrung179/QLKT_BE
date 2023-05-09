879048673e9a7d0fccde357e26bc6db43b0fae61
VU THANH TRUNG <54615459+trungquanadg@users.noreply.github.com>
2023-05-10 01:16:37 +0700

    update danh sach user

--- src/main/java/datn/qlkt/dto/dto/ExportDto.java
+++ src/main/java/datn/qlkt/dto/dto/ExportDto.java
@@ -13,17 +13,21 @@ public class ExportDto {
     private Date inTime;
     private String note;
     private List<WareHouseExportDto> wareHouseExport;
+    private Long moneyTotal;
+    private String creator;
     private Integer is_active;
 
     public ExportDto() {
     }
 
-    public ExportDto(Long id, String idExport, Date inTime, String note, List<WareHouseExportDto> wareHouseExport, Integer is_active) {
+    public ExportDto(Long id, String idExport, Date inTime, String note, List<WareHouseExportDto> wareHouseExport, Long moneyTotal, String creator, Integer is_active) {
         this.id = id;
         this.idExport = idExport;
         this.inTime = inTime;
         this.note = note;
         this.wareHouseExport = wareHouseExport;
+        this.moneyTotal = moneyTotal;
+        this.creator = creator;
         this.is_active = is_active;
     }
 }
