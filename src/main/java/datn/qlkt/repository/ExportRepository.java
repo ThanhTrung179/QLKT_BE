408eb4fc9ecccab3cd012ed4963f1761f5216107
VU THANH TRUNG <54615459+trungquanadg@users.noreply.github.com>
2023-05-24 16:22:41 +0700

    update sale

--- src/main/java/datn/qlkt/repository/ExportRepository.java
+++ src/main/java/datn/qlkt/repository/ExportRepository.java
@@ -61,6 +61,7 @@ public interface ExportRepository extends JpaRepository<Export, Long> {
             "AND (:idExport IS NULL OR e.idExport LIKE %:idExport%) " +
             "AND (:nameProduct IS NULL OR p.productName LIKE %:nameProduct%) " +
             "AND (:nameProducer IS NULL OR pr.producerName LIKE %:nameProducer%) " +
+            "AND (:creator IS NULL OR e.creator LIKE %:creator%) " +
             "AND (:startDate IS NULL OR :endDate IS NULL OR e.inTime BETWEEN :startDate AND :endDate)",
             countQuery = "SELECT COUNT(DISTINCT e) FROM Export e " +
                     "LEFT JOIN e.wareHouseExports w " +
@@ -70,10 +71,11 @@ public interface ExportRepository extends JpaRepository<Export, Long> {
                     "AND (:idExport IS NULL OR e.idExport LIKE %:idEntry%) " +
                     "AND (:nameProduct IS NULL OR p.productName LIKE %:nameProduct%) " +
                     "AND (:nameProducer IS NULL OR pr.producerName LIKE %:nameProducer%) " +
+                    "AND (:creator IS NULL OR e.creator LIKE %:creator%) " +
                     "AND (:startDate IS NULL OR :endDate IS NULL OR e.inTime BETWEEN :startDate AND :endDate)")
-    Page<Export> getAllSaleExportList(Pageable pageable, String product, String nameProduct, String idExport, String nameProducer, Date startDate, Date endDate);
+    Page<Export> getAllSaleExportList(Pageable pageable, String nameProduct, String idExport, String nameProducer,String creator, Date startDate, Date endDate);
 
-    @Query(value = "SELECT e, w, p, pr FROM Export e " +
+    @Query(value = "SELECT SUM(e.moneyTotal) AS totalMoney, e, w, p, pr FROM Export e " +
             "LEFT JOIN FETCH e.wareHouseExports w " +
             "LEFT JOIN FETCH w.product p " +
             "LEFT JOIN FETCH p.producers pr " +
@@ -81,6 +83,7 @@ public interface ExportRepository extends JpaRepository<Export, Long> {
             "AND (:idExport IS NULL OR e.idExport LIKE %:idExport%) " +
             "AND (:nameProduct IS NULL OR p.productName LIKE %:nameProduct%) " +
             "AND (:nameProducer IS NULL OR pr.producerName LIKE %:nameProducer%) " +
+            "AND (:creator IS NULL OR e.creator LIKE %:creator%) " +
             "AND (:startDate IS NULL OR :endDate IS NULL OR e.inTime BETWEEN :startDate AND :endDate)")
     Long calculateTotalMoney(String nameProduct, String idExport, String nameProducer, String creator, Date startDate, Date endDate);
     @Modifying
