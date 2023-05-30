a3dd668cf4abbaab36a4879cb69de991d3e4bda6
VU THANH TRUNG <54615459+trungquanadg@users.noreply.github.com>
2023-05-30 21:39:13 +0700

    Update ExportRepository.java
    update

--- src/main/java/datn/qlkt/repository/ExportRepository.java
+++ src/main/java/datn/qlkt/repository/ExportRepository.java
@@ -61,7 +61,7 @@ public interface ExportRepository extends JpaRepository<Export, Long> {
             "AND (:idExport IS NULL OR e.idExport LIKE %:idExport%) " +
             "AND (:nameProduct IS NULL OR p.productName LIKE %:nameProduct%) " +
             "AND (:nameProducer IS NULL OR pr.producerName LIKE %:nameProducer%) " +
-            "AND (:creator IS NULL OR e.creator LIKE %:creator%) " +
+            "AND (:creator IS NULL OR e.creator = :creator%) " +
             "AND (:startDate IS NULL OR :endDate IS NULL OR e.inTime BETWEEN :startDate AND :endDate)",
             countQuery = "SELECT COUNT(DISTINCT e) FROM Export e " +
                     "LEFT JOIN e.wareHouseExports w " +
